05d733624548516ee92a1b301b9d540e3e20e831
chrish <chrishcodes@gmail.com>
2020-09-12 21:28:22 -0700

    added PizzaTypes controller, factory, migration, seeder

---
+++ app/Http/Controllers/PizzaTypesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PizzaTypes;
+use Illuminate\Http\Request;
+
+class PizzaTypesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PizzaTypes  $pizzaTypes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PizzaTypes $pizzaTypes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PizzaTypes  $pizzaTypes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PizzaTypes $pizzaTypes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PizzaTypes  $pizzaTypes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PizzaTypes $pizzaTypes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PizzaTypes  $pizzaTypes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PizzaTypes $pizzaTypes)
+    {
+        //
+    }
+}
---
+++ database/factories/PizzaTypesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PizzaTypes;
+use Faker\Generator as Faker;
+
+$factory->define(PizzaTypes::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
